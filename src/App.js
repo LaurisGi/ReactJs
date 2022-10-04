952918315178dc5a692d6c682e9efc32011e48dc
LaurisGi <laurynas.gipas@gmail.com>
2022-10-04 21:07:13 +0300

    first commit to master

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Codeacademy.lt React paskaita!
         </p>
+        <form>
+          <input type='text'></input>
+          <button type='send'></button>
+        </form>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://codeacademy.online"
           target="_blank"
           rel="noopener noreferrer"
         >
