3eae1be242c7a34b875abf81a3a73c09727d2294
LaurisGi <laurynas.gipas@gmail.com>
2022-10-20 20:22:32 +0300

    1.1 commit

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,7 +1,7 @@
 export const Header = () => {
     return (
         <header>
-            <h1 className="mt-4 p5 bg-primary text-white rounded">Header</h1>
+            <h1 className="mt-4 p5 bg-primary text-white rounded" style={{padding: "10px 15px"}}>Header</h1>
         </header>
     )
 }
