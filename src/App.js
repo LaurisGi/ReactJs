00809a59948666f5d0180912448eef7bd79059cb
LaurisGi <laurynas.gipas@gmail.com>
2022-10-25 20:09:05 +0300

    paskaita 6 2 dalis

--- src/App.js
+++ src/App.js
@@ -3,49 +3,38 @@ import './App.css';
 
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
   useEffect(() => {
-    console.log('COMPONENT MOUNTED');
-  }, [count]);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((data)=> {
+      setPosts(data);
+    })
+  }, []);
 
-  const handleIncrement = (event) => {
-    console.log(event);
-  }
+  console.log(posts);
 
-  const handleIncrementKeyDown = (event) => {
-    console.log(event);
-  }
-
-  const handleIncrementKeyUp = (event) => {
-    console.log(event);
-  }
-  const handleInputChange = (event) => {
-    console.log(event.target.value);
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('SUBMIT');
-  }
-
-  const handleInputBlur = (event) => {
-    console.log('INPUT LOST FOCUS')
+  const handleOnChange = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
   }
 
   return (
-    <form action="" onSubmit={handleSubmit}>
       <div className='App'>
-        {count}
-        <button
-          onKeyUp={handleIncrementKeyUp}
-          onKeyDown={handleIncrementKeyDown}
-          onClick={handleIncrement}
-          >
-          Increment</button>
-          <input onChange={handleInputChange} onBlur={handleInputBlur}/>
+        <input className='form-control-lg form-control form-outline' onChange={handleOnChange}/>
+        {posts
+          .filter((post) => {
+            return post.title.indexOf(inputValue) >= 0
+          })
+          .map((post)=> (
+            <div key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+        ))}
       </div>
-    </form>
   );
 }
 
